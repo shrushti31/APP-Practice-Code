dd7738deefab56ff6ed7b5e687e2547967ae8af2
shrushti31 <shrushtizurunge@gmail.com>
2026-07-22 10:05:20 +0530

    Add files via upload

---
+++ Iterators.py
@@ -0,0 +1,23 @@
+# Custom Iterator Example
+
+class Numbers:
+
+    def __init__(self, start, end):
+        self.current = start
+        self.end = end
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current <= self.end:
+            value = self.current
+            self.current += 1
+            return value
+        else:
+            raise StopIteration
+
+num = Numbers(1, 5)
+
+for i in num:
+    print(i)
